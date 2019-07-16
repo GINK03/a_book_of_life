4f32515a725d2a75fa659eb5061310e754696229
Your Name <gim.kobayashi@gmail.com>
2019-07-17 00:26:05 +0900

    updat

--- MakeBookReadMatrix/C001.py
+++ MakeBookReadMatrix/C001.py
@@ -11,21 +11,27 @@ source_idx = {s: idx for idx, s in enumerate(source)}
 target_idx = {t: idx for idx, t in enumerate(target)}
 
 files = glob.glob('./works/each_shelve/*')
-Csr = lil_matrix((len(files), len(target_idx)))
+size = len(files)
+files_train = files[:int(0.98*size)]
+files_test = files[int(0.98*size):]
+CsrTrain = lil_matrix((len(files_train), len(target_idx)))
+CsrTest = lil_matrix((len(files_test), len(target_idx)))
 
-fns = []
-for idx, fn in enumerate(files):
-    obj = json.load(open(fn))
-    books = obj['books']
-    # print(books)
-    fns.append(fn)
-    for book in books:
-        try:
-            book = re.search(r'『(.*?)』', book).group(1)
-            Csr[idx, target_idx[Sanitize.sanitize(book)]] = 1
-        except Exception as ex:
-            print(idx, ex, book)
 
-ser = pickle.dumps((fns,Csr))
+fnsTrain, fnsTest = [], []
+for type, fns, Csr, files in [('train', fnsTrain, CsrTrain, files_train), ('test', fnsTest, CsrTest, files_test)]:
+    for idx, fn in enumerate(files):
+        obj = json.load(open(fn))
+        books = obj['books']
+        # print(books)
+        fns.append(fn)
+        for book in books:
+            try:
+                book = re.search(r'『(.*?)』', book).group(1)
+                Csr[idx, target_idx[Sanitize.sanitize(book)]] = 1
+            except Exception as ex:
+                print(idx, ex, book)
+
+ser = pickle.dumps((fnsTrain, CsrTrain, fnsTest, CsrTest))
 with open('ser.pkl', 'wb') as fp:
     fp.write(ser)
--- MakeBookReadMatrix/D001.py
+++ MakeBookReadMatrix/D001.py
@@ -8,16 +8,22 @@ import numpy as np
 import sys
 from pathlib import Path
 import re
-
+from sklearn.metrics import mean_squared_error
 if '--fit' in sys.argv:
-    fns, csr = pickle.load(open('ser.pkl', 'rb'))
+    fnsTrain, csrTrain, fnsTest, csrTest = pickle.load(open('ser.pkl', 'rb'))
     print('fit non-negative matrix factorization')
-    model = NMF(n_components=30, max_iter=600, init='random', random_state=0)
-    model.fit(csr)
+    model = NMF(n_components=20, max_iter=600, init='random', random_state=0)
+    model.fit(csrTrain)
+
+    yhat = np.dot(model.transform(csrTest), model.components_)
+    print(yhat.shape)
+    mse = mean_squared_error(csrTest.todense(), yhat)
+    print(f'test mse = {mse:0.06f}')
+
     with open('./works/models.pkl', 'wb') as fp:
         pickle.dump(model, fp)
-assert Path('./works/models.pkl').exists(), "there is not model."
 
+assert Path('./works/models.pkl').exists(), "there is not model."
 
 with open('./works/models.pkl', 'rb') as fp:
     model = pickle.load(fp)
@@ -33,25 +39,24 @@ for sidx, source_json in enumerate(['./sort_source_books.json', '../MakeBookRead
                 print(ex)
                 ...
 
-
-    target=json.load(open('./sort_target_books.json'))
-    target_idx={t: idx for idx, t in enumerate(target)}
-    Csr=lil_matrix((1, len(target_idx)))
+    target = json.load(open('./sort_target_books.json'))
+    target_idx = {t: idx for idx, t in enumerate(target)}
+    Csr = lil_matrix((1, len(target_idx)))
     for book in source:
         try:
-            Csr[0, target_idx[Sanitize.sanitize(book)]]=1
+            Csr[0, target_idx[Sanitize.sanitize(book)]] = 1
         except Exception as ex:
             print(ex)
 
-    P=model.transform(Csr)
-    Q=model.components_
+    P = model.transform(Csr)
+    Q = model.components_
     print(len(np.dot(P, Q)[-1]))
 
-    pq=np.dot(P, Q)[-1]
-    idx_target={idx: target for target, idx in target_idx.items()}
-    objs=[]
+    pq = np.dot(P, Q)[-1]
+    idx_target = {idx: target for target, idx in target_idx.items()}
+    objs = []
     for idx, target in idx_target.items():
-        obj={'book': target, 'score': pq[idx]}
+        obj = {'book': target, 'score': pq[idx]}
         objs.append(obj)
-    df=pd.DataFrame(objs).sort_values(by=['score'], ascending=False)
+    df = pd.DataFrame(objs).sort_values(by=['score'], ascending=False)
     df.to_csv(f'scores_{sidx:02d}.csv', index=None)
