5aa12d0d3668f2a86a361ac72c7551dda82c27b8
Your Name <gim.kobayashi@gmail.com>
2019-07-20 17:20:27 +0900

    add exception to kindle-fiona

--- KindleReadBooks/A001_from_kindle.py
+++ KindleReadBooks/A001_from_kindle.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains 
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver import DesiredCapabilities
 import bs4
@@ -17,14 +17,12 @@ import os
 import random
 from pathlib import Path
 PASSWORD = os.environ['PASSWORD']
-
+EMAIL = os.environ['EMAIL']
 
 def random_sleep():
     time.sleep(random.random())
 
-
 Path('outputs').mkdir(exist_ok=True)
-
 
 def run():
     options = Options()
@@ -51,7 +49,8 @@ def run():
     driver.save_screenshot('1.png')
 
     try:
-        driver.find_element_by_id("ap_email").send_keys('gim.kobayashi@gmail.com')
+        driver.find_element_by_id("ap_email").send_keys(
+            EMAIL)
         driver.find_element_by_id("ap_password").send_keys(PASSWORD)
         driver.find_element_by_id("signInSubmit").click()
     except Exception as ex:
@@ -62,20 +61,27 @@ def run():
         driver.find_element_by_id("signInSubmit").click()
     except Exception as ex:
         print(ex)
+    
+    driver.get('https://amazon.co.jp/gp/digital/fiona/manage')
+    time.sleep(5.) 
+    driver.save_screenshot('2.png')
     start = time.time()
     for i in range(20):
         print(f'now iter {i} elapsed={time.time()-start}')
         random_sleep()
         start = time.time()
         wait = WebDriverWait(driver, 300)
-        elm = wait.until(EC.visibility_of_element_located(
-            (By.XPATH, '//div[contains(@ng-show,"paginationThresholdReached")]//button')))
+        try:
+            elm = wait.until(EC.visibility_of_element_located(
+                (By.XPATH, '//div[contains(@ng-show,"paginationThresholdReached")]//button')))
 
-        driver.save_screenshot(f'outputs/fiona_{i:02d}.png')
-        html = driver.page_source
-        open(f'outputs/books_{i:02d}.html', 'w').write(html)
-        # driver.find_element_by_xpath('//div[contains(@ng-show,"paginationThresholdReached")]//button').click()
-        elm.click()
+            driver.save_screenshot(f'outputs/fiona_{i:02d}.png')
+            html = driver.page_source
+            open(f'outputs/books_{i:02d}.html', 'w').write(html)
+            # driver.find_element_by_xpath('//div[contains(@ng-show,"paginationThresholdReached")]//button').click()
+            elm.click()
+        except Exception as ex:
+            break
 
 
 if __name__ == '__main__':
